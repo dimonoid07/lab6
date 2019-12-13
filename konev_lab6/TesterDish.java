8a52ad60dbd95ad4db11ee34efe32a7294e98c7e
dimonoid07 <55837857+dimonoid07@users.noreply.github.com>
2019-12-13 22:57:44 +0300

    Add files via upload

---
+++ konev_lab6/TesterDish.java
@@ -0,0 +1,8 @@
+package konev_lab6;
+
+public class TesterDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("металлическая", "серебрянный", "маленькая");
+        System.out.println(t);
+    }
+}
